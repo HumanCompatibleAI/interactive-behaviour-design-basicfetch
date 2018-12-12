cc76886a26f057177c32b1db87f90811ae1b01bb
Matthew Rahtz <matthew.rahtz@gmail.com>
2018-12-11 16:50:33 -0800

    Just use reward vector

--- basicfetch.py
+++ basicfetch.py
@@ -9,12 +9,11 @@ from gym.wrappers import FlattenDictWrapper
 
 
 class FetchEnvBasic(RobotEnv, EzPickle):
-    def __init__(self, target, reward):
-        self.target_type = target
-        self.reward = reward
+    def __init__(self):
+        self.r_vec = np.array([0, 0, 0])
         model_path = os.path.join(os.path.dirname(__file__), 'mujoco-py/xmls/fetch/main.xml')
         RobotEnv.__init__(self, model_path=model_path, n_substeps=20, n_actions=8, initial_qpos=None)
-        EzPickle.__init__(self, target, reward)
+        EzPickle.__init__(self)
 
     def get_ctrl_names(self):
         return self.sim.model.actuator_names
@@ -56,34 +55,10 @@ class FetchEnvBasic(RobotEnv, EzPickle):
         return False
 
     def compute_reward(self, achieved_goal, desired_goal, info):
-        if self.target_type == 'level':
-            assert achieved_goal.shape == (4,), achieved_goal.shape
-            assert desired_goal.shape == (4,), desired_goal.shape
-            diff = np.linalg.norm(achieved_goal - desired_goal)
-            return -diff
-        elif self.target_type in ['up', 'right']:
-            assert isinstance(achieved_goal, np.float64), type(desired_goal)
-            assert isinstance(desired_goal, np.float64), type(desired_goal)
-            if self.reward == 'sparse':
-                return float(achieved_goal > desired_goal)
-            elif self.reward == 'dense':
-                capped_distance = max(desired_goal - achieved_goal, 0)
-                return -capped_distance
-            elif self.reward == 'max':
-                return achieved_goal
-            else:
-                raise Exception(f"Unknown reward type '{self.reward}'")
-        else:
-            raise Exception(f"Unknown reward target '{self.target_type}'")
+        return np.dot(achieved_goal, self.r_vec)
 
     def _sample_goal(self):
-        if self.target_type == 'level':
-            return np.array([1., 0., 1., 0.])
-        elif self.target_type in ['up', 'right']:
-            # np.float64 because when training with baselines something wants to call copy()
-            return np.float64(0.8)
-        else:
-            raise Exception(f"Unknown reward target '{self.target_type}'")
+        return np.array((0, 0, 0))
 
     def _get_obs(self):
         # gripper position
@@ -99,17 +74,9 @@ class FetchEnvBasic(RobotEnv, EzPickle):
 
         obs = np.concatenate([gripper_pos, gripper_vel, fingers_distance, fingers_vel])
 
-        if self.target_type == 'up':
-            achieved = gripper_pos[2]
-        elif self.target_type == 'right':
-            achieved = gripper_pos[1]
-        elif self.target_type == 'level':
-            achieved = self.sim.data.get_body_xquat('gripper_link')
-        else:
-            raise Exception(f"Unknown reward target '{self.reward}'")
         return {
             'observation': obs.copy(),
-            'achieved_goal': achieved,
+            'achieved_goal': gripper_pos,
             'desired_goal': self.goal
         }
 
@@ -125,19 +92,16 @@ class StartWithExplore(Wrapper):
         return self.env.step(action)
 
 
-def make_env(target, reward):
-    env = FetchEnvBasic(target, reward)
+def make_env():
+    env = FetchEnvBasic()
     env = FlattenDictWrapper(env, ['observation'])
     env = StartWithExplore(env)
     return env
 
 
 def register():
-    for reward in ['sparse', 'dense', 'max']:
-        for target in ['up', 'right', 'level']:
-            gym_register(
-                id=f'FetchBasic{target.capitalize()}{reward.capitalize()}-v0',
-                entry_point=make_env,
-                max_episode_steps=250,
-                kwargs={'target': target, 'reward': reward}
-            )
+    gym_register(
+        id=f'FetchBasic-v0',
+        entry_point=make_env,
+        max_episode_steps=250
+    )
