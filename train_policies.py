e3ba7b5378b05a6772080af03a6569396d1ef514
Matthew Rahtz <matthew.rahtz@gmail.com>
2018-12-11 17:03:05 -0800

    Train in subprocesses

--- train_policies.py
+++ train_policies.py
@@ -2,6 +2,9 @@
 import argparse
 import os
 import subprocess
+from multiprocessing import Process
+
+from tmuxprocess import TmuxProcess
 
 parser = argparse.ArgumentParser()
 parser.add_argument('runs_dir')
@@ -9,6 +12,10 @@ args = parser.parse_args()
 
 names = ['up', 'down', 'left', 'right', 'forward', 'backward']
 vecs = ['0 0 1', '0 0 -1', '1 0 0', '-1 0 0', '0 1 0', '0 -1 0']
+procs = []
 for name, vec in zip(names, vecs):
     dir = os.path.join(args.runs_dir, 'FetchBasic' + name.capitalize())
-    subprocess.run(['python', 'train.py', dir, vec])
+    p = TmuxProcess(target=subprocess.run, args=[['python', 'train.py', dir, vec]])
+    p.start()
+    procs.append(p)
+subprocess.run(f'tmux attach -t {procs[0].tmux_sess}'.split(' '))
