521c91fe70a830a0b4ed278be75be2213f1eec8b
Matthew Rahtz <matthew.rahtz@gmail.com>
2018-12-13 10:08:20 -0800

    Switch action mechanism and check for left/right/up/down

--- demo_manual.py
+++ demo_manual.py
@@ -1,19 +1,12 @@
-import select
-import sys
+import readline
 
 import gym
 import numpy as np
-from gym.envs.robotics.rotations import quat2euler
 
 from basicfetch import register
 
-
-def read_timeout(timeout=(1 / 30)):
-    i, o, e = select.select([sys.stdin], [], [], timeout)
-    if i:
-        return sys.stdin.readline().strip()
-    else:
-        return None
+# noinspection PyStatementEffect
+readline
 
 register()
 env = gym.make('FetchBasic-v0').unwrapped  # unwrap past TimeLimit
@@ -21,23 +14,35 @@ env.r_vec = np.array((1, 0, 0))
 np.set_printoptions(1)
 print(env.get_ctrl_names())
 while True:
-    command = read_timeout()
-    action = np.zeros(env.action_space.shape)
-    if command is None:
+    command = input()
+    try:
+        action = np.fromstring(command, sep=' ')
+        if len(action) != env.action_space.shape[0] + 1:
+            raise Exception("Wrong length")
+    except Exception as e:
+        print(e)
+        continue
+    action, reps = action[:-1], int(action[-1])
+    print(action, reps)
+
+    for _ in range(reps):
         obs, reward, done, info = env.step(action)
+        print(reward)
         env.render(mode='human')
-    else:
-        try:
-            action_idx, val, reps = command.split(' ')
-            action_idx = int(action_idx)
-            val = float(val)
-            reps = int(reps)
-            action[action_idx] = val
-            for _ in range(reps):
-                obs, reward, done, info = env.step(action)
-                print(reward)
-                env.render(mode='human')
-                print('Grippper position:', env.sim.data.get_site_xpos('grip'))
-                print('Reward:', reward)
-        except Exception as e:
-            print(e)
+        print('Grippper position:', env.sim.data.get_site_xpos('grip'))
+        print('Reward:', reward)
+
+        # left side of table: x 1.073 to 1.435, y 0.4
+        # right side of table: x 1.044 to 1.424, y 1.09
+        # back side of table: x 1.023, y 0.424 to 1.101
+        # front side of table: x 1.424, y 0.402 to 1.093
+        pos = env.unwrapped.sim.data.get_site_xpos('grip')
+        e = 0.05
+        if pos[0] > 1.044 - e and pos[0] < 1.435 + e and pos[1] > 0.4 - e and pos[1] < 0.4 + e:
+            print('left')
+        if pos[0] > 1.044 - e and pos[0] < 1.435 + e and pos[1] > 1.09 - e and pos[1] < 1.09 + e:
+            print('right')
+        if pos[0] > 1.023 - e and pos[0] < 1.023 + e and pos[1] > 0.402 - e and pos[1] < 1.101 + e:
+            print('back')
+        if pos[0] > 1.5 - e and pos[0] < 1.5 + e and pos[1] > 0.402 - e and pos[1] < 1.101 + e:
+            print('front')
