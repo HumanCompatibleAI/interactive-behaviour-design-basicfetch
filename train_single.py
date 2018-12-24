06d04ffb6543988934d635ff9c983fb378991e0a
Matthew Rahtz <matthew.rahtz@gmail.com>
2018-12-24 11:58:54 -0800

    Switch to DDPG

--- train_single.py
+++ train_single.py
@@ -76,8 +76,8 @@ register(
 baselines.run._game_envs['robotics'].add('E-v0')
 # 1e5 total timesteps on 16 workers is about 5 minutes
 # 1e6 is about an hour
-arg_str = f"--alg=ppo2 --env=E-v0 --num_env {args.n_envs} --nsteps 128 --num_timesteps 1e6 --seed {args.seed} "
+arg_str = f"--alg=ddpg --env=E-v0 --num_timesteps 1e6 --seed {args.seed} "
 arg_str += f"--save_path {os.path.join(args.dir, 'saved_model')} --log_interval 3 --save_interval 10"
 arg_str += f" --load_path {args.ckpt}"
-sys.argv = arg_str.split(" ")
+sys.argv = [sys.argv[0]] + arg_str.split(" ")
 baselines_run_main()
