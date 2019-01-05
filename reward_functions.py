789d010acc49143c21313beb6c99064c2ce5b56a
Matthew Rahtz <matthew.rahtz@gmail.com>
2019-01-05 14:55:36 -0800

    Re-enable all reward functions

--- reward_functions.py
+++ reward_functions.py
@@ -52,28 +52,27 @@ v = {
     'up': [0, 0, 1],
     'down': [0, 0, -1]
 }
-# reward_function_dict['direction'] = {}
-# reward_function_dict['goal'] = {}
-# reward_function_dict['cosangle'] = {}
+reward_function_dict['direction'] = {}
+reward_function_dict['goal'] = {}
+reward_function_dict['cosangle'] = {}
 reward_function_dict['cosanglelimit'] = {}
 for dir_name, dir_vector in v.items():
-    # reward_function_dict['direction'][dir_name] = lambda quat, pos: np.dot(pos, dir_vector)
-    # reward_function_dict['goal'][dir_name] = goal_direction(dir_vector)
-    # reward_function_dict['cosangle'][dir_name] = cos_angle(dir_vector)
-    reward_function_dict['cosanglelimit'][dir_name] = cos_angle(dir_vector,
-                                                                limit_degrees=10)
+    reward_function_dict['direction'][dir_name] = lambda quat, pos: np.dot(pos, dir_vector)
+    reward_function_dict['goal'][dir_name] = goal_direction(dir_vector)
+    reward_function_dict['cosangle'][dir_name] = cos_angle(dir_vector)
+    reward_function_dict['cosanglelimit'][dir_name] = cos_angle(dir_vector, limit_degrees=10)
 
-# reward_function_dict['table_edges'] = {}
-# above_table = lambda pos: pos[2] > 0.42
-# e = 0.05
-# reward_function_dict['table_edges']['left'] = lambda quat, pos: float(
-#     above_table(pos) and pos[0] > 1.044 - e and pos[0] < 1.435 + e and pos[1] > 0.4 - e and pos[1] < 0.4 + e)
-# reward_function_dict['table_edges']['right'] = lambda quat, pos: float(
-#     above_table(pos) and pos[0] > 1.044 - e and pos[0] < 1.435 + e and pos[1] > 1.09 - e and pos[1] < 1.09 + e)
-# reward_function_dict['table_edges']['back'] = lambda quat, pos: float(
-#     above_table(pos) and pos[0] > 1.023 - e and pos[0] < 1.023 + e and pos[1] > 0.402 - e and pos[1] < 1.101 + e)
-# reward_function_dict['table_edges']['front'] = lambda quat, pos: float(
-#     above_table(pos) and pos[0] > 1.5 - e and pos[0] < 1.5 + e and pos[1] > 0.402 - e and pos[1] < 1.101 + e)
-#
-# reward_function_dict['orientation'] = lambda quat, pos: float(
-#     all(np.isclose(quat, [np.sqrt(0.5), 0, np.sqrt(0.5), 0], atol=0.1)))
+reward_function_dict['table_edges'] = {}
+above_table = lambda pos: pos[2] > 0.42
+e = 0.05
+reward_function_dict['table_edges']['left'] = lambda quat, pos: float(
+    above_table(pos) and pos[0] > 1.044 - e and pos[0] < 1.435 + e and pos[1] > 0.4 - e and pos[1] < 0.4 + e)
+reward_function_dict['table_edges']['right'] = lambda quat, pos: float(
+    above_table(pos) and pos[0] > 1.044 - e and pos[0] < 1.435 + e and pos[1] > 1.09 - e and pos[1] < 1.09 + e)
+reward_function_dict['table_edges']['back'] = lambda quat, pos: float(
+    above_table(pos) and pos[0] > 1.023 - e and pos[0] < 1.023 + e and pos[1] > 0.402 - e and pos[1] < 1.101 + e)
+reward_function_dict['table_edges']['front'] = lambda quat, pos: float(
+    above_table(pos) and pos[0] > 1.5 - e and pos[0] < 1.5 + e and pos[1] > 0.402 - e and pos[1] < 1.101 + e)
+
+reward_function_dict['orientation'] = lambda quat, pos: float(
+    all(np.isclose(quat, [np.sqrt(0.5), 0, np.sqrt(0.5), 0], atol=0.1)))
